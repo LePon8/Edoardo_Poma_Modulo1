701477fb364874ddec6f490c4b1b855c36527230
LePon <edoardopoma98@gmail.com>
2023-02-23 12:16:21 +0100

    Limits_Bullet

--- Assets/Scripts/Character_Controller.cs
+++ Assets/Scripts/Character_Controller.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Character_Controller : MonoBehaviour
 {
     [SerializeField] float speed;
+    [SerializeField] float limits;
+
     Rigidbody rb;
 
     // Start is called before the first frame update
@@ -17,6 +19,7 @@ public class Character_Controller : MonoBehaviour
     void Update()
     {
         Movement();
+        Limits();
     }
 
     void Movement()
@@ -28,5 +31,26 @@ public class Character_Controller : MonoBehaviour
 
         //Modifico la posizione del rb
         rb.MovePosition(transform.position + movement * Time.deltaTime * speed);
+    }
+
+    void Limits()
+    {
+        if(transform.position.y >= limits)
+        {
+            transform.position = new Vector2(gameObject.transform.position.x, limits);
+        }
+        else if(gameObject.transform.position.y <= -limits)
+        {
+            gameObject.transform.position = new Vector2(gameObject.transform.position.x, -limits);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Limit"))
+        {
+            Debug.Log("Yeppa");
+            //Time.timeScale = 0;
+        }
     }
 }
